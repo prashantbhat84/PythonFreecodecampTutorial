c1b42e9ce03370241ab2b3431b59f98537dcce8b
Prashant Bhat <prashantbhat84@gmail.com>
2024-07-13 08:38:26 +0000

    creating a function and calling it

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-player_choice = 'rick'
+
+# create a function 
+def get_choices():
+  player_choice = "rock"
+  computer_choice = "paper"
+
+  return computer_choice +''+ player_choice
+# calling a function
+choices = get_choices()
+print(choices)
